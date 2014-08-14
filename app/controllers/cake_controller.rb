2acd28cd87181f27b7e20742331189700a7b67ce
nealfennimore <hi@neal.codes>
2014-08-14 13:59:00 -0400

    Added data attribute to html in order to use new age methods.
    - Age takes in a new Time object and then returns how old the user is.
    - That age instance variable is then stored in the HTML

--- app/controllers/cake_controller.rb
+++ app/controllers/cake_controller.rb
@@ -1,2 +1,13 @@
 class CakeController < ApplicationController
+
+  def age(dob)
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
+  def index
+    @age = age(Time.new(1987,8,31))
+  end
+
+
 end
