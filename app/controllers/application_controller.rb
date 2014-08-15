c862bc5e81f290563c89866c762ef93ae9ab1326
nealfennimore <hi@neal.codes>
2014-08-14 21:03:01 -0400

    Added age method to application controller - moved from cake. Updated methods for user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def age(dob)
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+  
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,31 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+  end
+
+  def ajax
+    @users = User.all
+    @age = age(Time.new(1987,8,31))
     render layout: false
   end
 
-  def create
-    User.create(user_params)
-  end
-
-  def new
-    @user = User.new
+  def show
+    @user = User.find(current_user.id)
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
 
   def update
+    User.find(current_user.id).update(user_params)
+    redirect_to user_path(current_user.id)
   end
 
   def destroy
+    User.find(current_user.id).destroy
+    session[:user_id] = nil
+    redirect_to root_url
   end
 
   private
